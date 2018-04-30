183e2a163b407b424a9f2aecd7ffa06118db28e3
wjdru <wjdrudtn0225@naver.com>
2018-04-30 15:53:16 +0900

    two

---
+++ src/component/Navigate.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Button} from 'semantic-ui-react';
+import './Navigate.css'
+
+const Navigate = () => (
+    <div className="Navigate">
+        <Button
+            color="teal"
+            content="Previous"
+            icon="left arrow"
+        />
+)
